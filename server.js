7abdbe7759431774f337246d3302aecdea7c41ac
Jim Rottinger <jamesrottinger@gmail.com>
2018-08-13 13:48:20 -0700

    Serve static JS and html files from server.js

--- server.js
+++ server.js
@@ -1,27 +1,39 @@
 const http = require('http');
+const fileSystem = require('fs');
+const path = require('path');
 
 const hostname = 'localhost';
 const host_port = 8000;
 const client_port = 8001;
 
+function serveAsset(rootPath, url, res) {
+  // default root route to index.html in the folder
+  if (url === '/') url = 'index.html';
+
+  const filePath = path.join(__dirname, rootPath, url)
+  const readStream = fileSystem.createReadStream(filePath)
+    .on('error', function() {
+      res.statusCode = 404;
+      res.end();
+    });
+
+  if (/^.*\.js$/.test(url)) {
+    res.setHeader('Content-Type', 'text/javascript');
+  } else {
+    res.setHeader('Content-Type', 'text/html');
+  }
+
+  readStream.pipe(res);
+}
+
 const host_server = http.createServer((req, res) => {
-  debugger
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+  serveAsset('host', req.url, res)
+}).listen(host_port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${host_port}/`);
+});;
 
 const client_server = http.createServer((req, res) => {
-  console.log(req)
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Client\n');
-});
-
-host_server.listen(host_port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${host_port}/`);
-});
-
-client_server.listen(client_port, hostname, () => {
+  serveAsset('client', req.url, res)
+}).listen(client_port, hostname, () => {
   console.log(`Client running at http://${hostname}:${client_port}/`);
 });
